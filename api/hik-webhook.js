f78b36a13930ac31211d44f7ec208f5932e31ee6
abhijitvborase <abhijitvborase@gmail.com>
2025-12-03 05:20:39 +0000

    hikvision webhook added

--- api/hik-webhook.js
+++ api/hik-webhook.js
@@ -1,14 +1,17 @@
-module.exports = (req, res) => {
-    console.log("Webhook call:", req.method);
+// api/hik-webhook.js  (ES module)
+
+export default function handler(req, res) {
+    console.log("Hik webhook test, method =", req.method);
   
     if (req.method === "GET") {
       return res.status(200).json({ status: "ok-get", msg: "webhook working" });
     }
   
     if (req.method === "POST") {
+      console.log("Body:", req.body);
       return res.status(200).json({ status: "ok-post", msg: "posted" });
     }
   
     return res.status(405).json({ message: "Method Not Allowed" });
-  };
+  }
   
