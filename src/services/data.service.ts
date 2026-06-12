1d41503fb224ec71e2f9f6fbd9d9a2bf5452df01
abhijitvborase <abhijitvborase@gmail.com>
2026-06-12 14:48:08 +0000

    Debug offline cache

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -138,12 +138,20 @@ export class DataService {
   }
   
   private loadOfflineCache() {
-    console.log(
-      'CACHE LOADED',
-      this._employees().length,
-      this._coupons().length,
-      this._menus().length
-    );
+    const employees = localStorage.getItem('employees_cache');
+const coupons = localStorage.getItem('coupons_cache');
+const menus = localStorage.getItem('menus_cache');
+alert(
+  'EMP=' +
+  (employees ? JSON.parse(employees).length : 0) +
+  ' COUP=' +
+  (coupons ? JSON.parse(coupons).length : 0)
+);
+console.log('CACHE RAW', {
+  employeesLength: employees ? JSON.parse(employees).length : 0,
+  couponsLength: coupons ? JSON.parse(coupons).length : 0,
+  menusLength: menus ? JSON.parse(menus).length : 0,
+});
     try {
       const employees = localStorage.getItem('employees_cache');
       const coupons = localStorage.getItem('coupons_cache');
